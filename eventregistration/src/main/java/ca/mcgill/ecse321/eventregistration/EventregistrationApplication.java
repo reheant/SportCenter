7ef7bacf0732e7a9554dd8dfc5939284ace3fa88
reheant <rehean.thillai@gmail.com>
2024-01-25 15:24:19 -0500

    backend from tutorial

---
+++ eventregistration/src/main/java/ca/mcgill/ecse321/eventregistration/EventregistrationApplication.java
@@ -0,0 +1,13 @@
+package ca.mcgill.ecse321.eventregistration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventregistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventregistrationApplication.class, args);
+	}
+
+}
