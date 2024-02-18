e8b36af0e5f8fdf501a538cd258f07fc5cd51051
Marc-Antoine Nadeau <marc-antoine.nadeau@mail.mcgill.ca>
2024-02-18 17:28:24 -0500

    Revert "renaming sports center folder + all the stuff"
    This reverts commit a54c3a15ae8d6162bdab7b03d320a3a80d9748d2.

--- sportscenter/src/main/java/ca/mcgill/ecse321/sportscenter/dao/CourseRepository.java
+++
@@ -1,8 +0,0 @@
-package ca.mcgill.ecse321.sportscenter.dao;
-
-import org.springframework.data.repository.CrudRepository;
-import ca.mcgill.ecse321.sportscenter.model.Course;
-
-public interface CourseRepository extends CrudRepository<Course, Integer> {
-
-}
--- sportscenter/src/main/java/ca/mcgill/ecse321/sportscenter/dao/PayPalRepository.java
+++
@@ -1,8 +0,0 @@
-package ca.mcgill.ecse321.sportscenter.dao;
-
-import org.springframework.data.repository.CrudRepository;
-import ca.mcgill.ecse321.sportscenter.model.PayPal;
-
-public interface PayPalRepository extends CrudRepository<PayPal, Integer> {
-
-}
