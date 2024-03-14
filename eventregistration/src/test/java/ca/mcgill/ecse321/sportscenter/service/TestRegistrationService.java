49d9bd259be50f37a03079fed10e1ef8e20bd24b
jubiiz <jubiiz.audet@gmail.com>
2024-03-13 21:53:50 -0400

    WIP started adding registration test

--- eventregistration/src/test/java/ca/mcgill/ecse321/sportscenter/service/TestRegistrationService.java
+++ eventregistration/src/test/java/ca/mcgill/ecse321/sportscenter/service/TestRegistrationService.java
@@ -1,12 +1,80 @@
 package ca.mcgill.ecse321.sportscenter.service;
 
+import ca.mcgill.ecse321.sportscenter.dao.AccountRepository;
+import ca.mcgill.ecse321.sportscenter.dao.RegistrationRepository;
+import ca.mcgill.ecse321.sportscenter.model.Account;
+import ca.mcgill.ecse321.sportscenter.model.Customer;
+import ca.mcgill.ecse321.sportscenter.model.Registration;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.lenient;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.invocation.InvocationOnMock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.stubbing.Answer;
 
 @ExtendWith(MockitoExtension.class)
 public class TestRegistrationService {
     
+    @Mock
+    private AccountRepository accountRepository;
+    @Mock 
+    private RegistrationRepository registrationRepository;
+
     @InjectMocks
     private RegistrationService registrationService;
+
+    // For Customer
+    private static final String firstName = "Jim";
+    private static final String lastName = "Bob";
+    private static final String email = "jim.bob@gmail.com";
+    private static final String password = "Str0ngP4ssword";
+
+
+
+    private final Customer customer = new Customer();
+    private final Account customerAccount = new Account();
+
+
+    @BeforeEach
+    public void setMockOutput() {
+        lenient().when(accountRepository.findAccountByEmail(anyString())).thenAnswer((InvocationOnMock invocation) -> {
+            if(invocation.getArgument(0).equals(email)) {
+                customerAccount.setEmail(email);
+                customerAccount.setFirstName(firstName);
+                customerAccount.setLastName(lastName);
+                customerAccount.setPassword(password);
+                customer.setAccount(customerAccount);
+
+                return customerAccount;
+            } else {
+                return null;
+            }
+
+        });
+
+        // Whenever anything is saved, just return the parameter object
+        Answer<?> returnParameterAsAnswer = (InvocationOnMock invocation) -> {
+            return invocation.getArgument(0);
+        };
+        lenient().when(registrationRepository.save(any(Registration.class))).thenAnswer(returnParameterAsAnswer);
+    }
+
+    @Test
+    public void testCreateRegistration() {
+        
+        
+
+        try {
+            registrationService.register(email, session_id);
+        }
+
+    }
+
 }
