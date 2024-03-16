9d0f0f6c2db520d812159bfaaafcc9f0e22f753d
Marc-Antoine Nadeau <marc-antoine.nadeau@mail.mcgill.ca>
2024-03-16 18:25:24 -0400

    fixed Imports

--- eventregistration/src/main/java/ca/mcgill/ecse321/sportscenter/controller/CourseRestController.java
+++ eventregistration/src/main/java/ca/mcgill/ecse321/sportscenter/controller/CourseRestController.java
@@ -7,20 +7,19 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
-
-import ca.mcgill.ecse321.sportscenter.dto.CourseDto;
-import ca.mcgill.ecse321.sportscenter.model.Course;
-import ca.mcgill.ecse321.sportscenter.service.CourseService;
-
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
+
+import ca.mcgill.ecse321.sportscenter.dto.CourseDto;
+import ca.mcgill.ecse321.sportscenter.model.Course;
+import ca.mcgill.ecse321.sportscenter.service.CourseService;
 
 @CrossOrigin(origins = "*")
 @RestController
