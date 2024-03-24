a334b85a94cb051cc6699075a6d7cd49f5e0091f
reheant <rehean.thillai@gmail.com>
2024-03-24 15:41:46 -0400

    removing unused import

--- eventregistration/src/main/java/ca/mcgill/ecse321/sportscenter/controller/RegistrationRestController.java
+++ eventregistration/src/main/java/ca/mcgill/ecse321/sportscenter/controller/RegistrationRestController.java
@@ -3,7 +3,6 @@ package ca.mcgill.ecse321.sportscenter.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.ExceptionHandler;
