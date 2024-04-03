5945468e6a510b10513ce968df76e60643fc8f21
reheant <rehean.thillai@gmail.com>
2024-04-03 13:12:21 -0400

    wip

---
+++ eventregistration/SportCenter-Frontend/src/components/javascript/Admin.js
@@ -0,0 +1,11 @@
+import axios from 'axios'
+import config from '../../../config'
+
+const frontendUrl = 'http://' + config.dev.host + ':' + config.dev.port
+const backendUrl = 'http://' + config.dev.backendHost + ':' + config.dev.backendPort
+
+const AXIOS = axios.create({
+  baseURL: backendUrl,
+  headers: { 'Access-Control-Allow-Origin': frontendUrl }
+  
+})
