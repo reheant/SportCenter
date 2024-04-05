615187aa289bb42f80ad1a3fea633761c2beda8f
reheant <rehean.thillai@gmail.com>
2024-04-04 22:00:23 -0400

    navbar fix

--- eventregistration/SportCenter-Frontend/src/components/vue/CreateCourse.vue
+++ eventregistration/SportCenter-Frontend/src/components/vue/CreateCourse.vue
@@ -8,10 +8,11 @@
 
         <b-collapse id="nav-collapse" is-nav>
           <b-navbar-nav>
-            <b-nav-item @click="onReturn"
-              >View Courses</b-nav-item
-            >
-          </b-navbar-nav>
+                <b-nav-item to="/admin/viewCustomers" >View Customers</b-nav-item>
+                <b-nav-item to="/admin/viewInstructors" href="#">View Instructors</b-nav-item>
+                <b-nav-item to="/admin/displayCourse" href="#">View Courses</b-nav-item>
+                <b-nav-item to="/admin/createCourse" href="#">Create Course</b-nav-item>
+            </b-navbar-nav>
 
           <b-navbar-nav class="ml-auto">
             <b-nav-item-dropdown right>
