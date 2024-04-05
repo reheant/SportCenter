6db98489d89b68168a9bdc4be14d19ec1515af72
Marc-Antoine Nadeau <nadeau.marc-antoine@hotmail.com>
2024-04-04 20:02:35 -0400

    Fixed Imports

--- eventregistration/src/test/java/ca/mcgill/ecse321/sportscenter/controller/CourseRestControllerIntegrationTest.java
+++ eventregistration/src/test/java/ca/mcgill/ecse321/sportscenter/controller/CourseRestControllerIntegrationTest.java
@@ -7,7 +7,6 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import java.util.Optional;
 
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
